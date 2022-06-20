5f9bf28d0e3a4b23f046e2223d9bca8f53e84df2
Jorge Sisco <jorgesisco17@gmail.com>
2022-06-20 19:43:15 +0200

    partialy deploying the script on streamlit

--- Script_2.py
+++ Script_2.py
@@ -5,7 +5,7 @@ import camelot.io as camelot
 class ExtractPDFTables:
 
 	# Initializing contructor variables
-	def __init__(self, file_path, page_range=None, area=None, flavor=None, strip_text=None):
+	def __init__(self, file_path, page_range=None, area=None, flavor=None, strip_text=None, get_codes=None):
 		self.file_path = file_path #string with file path
 		self.page_range = page_range # defined page range with tables
 		self.area = area # Table area, libraris like tabula allows to specify table area
@@ -13,7 +13,11 @@ class ExtractPDFTables:
 		#Camelot parameters
 		self.flavor = flavor
 		self.strip_text = strip_text
-	
+
+		self.get_codes = get_codes
+    
+    
+
 
 	#Table Extrator with Camelot Library
 	def getTablesCamelot(self):
@@ -32,4 +36,41 @@ class ExtractPDFTables:
 				pass
 
 		df =  pd.concat(frames)
-		return df
+
+		if self.get_codes != None:
+			
+			
+			df = self.getCode(df, 
+                            column=3,
+                            regex='\d\d\d:',
+                            delimiter='\n',
+                            code_column=f'{self.get_codes[0]}_code',
+                            remove_char=':')
+			
+			
+			df = self.getCode(df, 
+                            column=2,
+                            regex='\w+.\d+\)',
+                            delimiter='\n',
+                            code_column=f'{self.get_codes[1]}_code',
+                            remove_char=')')
+			
+			
+			return df
+        
+		else:
+			return df
+    
+	def getCode(self, df,
+                    column,
+                    regex, 
+                    delimiter, 
+                    code_column,
+                    remove_char=None):
+                    
+                    df[code_column] = df[column].str.findall(regex).apply(set).str.join(delimiter)
+
+                    if remove_char != None:
+                        df[code_column] = df[code_column].str.replace(remove_char, '', regex=False)
+
+                    return df
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 import pandas as pd
 import numpy as np
 import base64
@@ -21,20 +22,34 @@ def gh():
 gh()
 
 st.title("ESG PDF Table Extractor")
-st.subheader("Developed by Jorge Sisco")
+st.subheader("Script currently working only with ADX-GRI16 PDF")
+st.text("Note: Only table extration from PDF words on this app, mapping on excel file will be\nimplemented later on")
+
 
 input_pdf = st.file_uploader(label = "upload your pdf here", type = 'pdf')
 
+
+get_codes_check = st.checkbox('Would you like to get columns with ESGs codes? (optional)')
+
+if get_codes_check:
+    st.markdown("### ESG #1")
+    esg_1 = st.text_input("Enter ESG Name:", placeholder='e.g: GRI_2016, ADX, SDG', value='GRI_16')
+
+    st.markdown("### ESG #2")
+    esg_2 = st.text_input("Enter ESG Name:", placeholder=' e.g: GRI_2016, ADX', value='ADX')
+
 st.markdown("### Page Number")
-page_number = st.text_input("Enter the page or pages:", placeholder='e.g: 1, 1-10')
+page_number = st.text_input("Enter the page or pages:", placeholder=' e.g: 1, 1-10', value='10-11')
 
 st.markdown('### Flavor Approach')
-flavor_ = st.text_input("Type 'lattice' for tables with visible gridlines or 'stream' for tables with not gridlines at all", value='lattice')
+flavor_ = st.text_input("Type 'lattice' for tables with visible gridlines or 'stream' for tables with not gridlines at all", placeholder='stream or lattice', value='lattice')
 
 
 Extract_Tables_button = st.button(label='Extract Tables', key=None, help=None, on_click=None, args=None, kwargs=None, disabled=False)
 
-if input_pdf != None and Extract_Tables_button:
+
+
+if input_pdf != None and  Extract_Tables_button:
 
     
 
@@ -47,20 +62,30 @@ if input_pdf != None and Extract_Tables_button:
 
         # read the pdf and parse it using stream
         # table = camelot.read_pdf("input.pdf", pages = page_number, flavor = flavor_)
-        table = ExtractPDFTables("input.pdf",
+        if get_codes_check:
+            df = ExtractPDFTables("input.pdf",
+                                    page_range = page_number,
+                                    flavor = flavor_,
+                                    strip_text = '\n',
+                                    get_codes=[esg_1, esg_2]).getTablesCamelot()
+            
+        else:
+            df = ExtractPDFTables("input.pdf",
                                 page_range = page_number,
                                 flavor = flavor_,
                                 strip_text = '\n').getTablesCamelot()
+        
+         
+        if len(df) > 0:
+
             
 
-        if len(table) > 0:
+            st.dataframe(df)
 
-            st.dataframe(table)
+            def convert_df(df):
+                return df.to_csv().encode('utf-8')
 
-            def convert_df(table):
-                return table.to_csv().encode('utf-8')
-
-            csv = convert_df(table)
+            csv = convert_df(df)
 
             st.download_button(
                  label="Download data as CSV",
